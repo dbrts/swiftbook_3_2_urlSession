15d78d38025e83626893c1801a7dde8cb799eb9d
Gennady Dobretsov <dobrets@gmail.com>
2022-11-06 12:54:51 +0600

    Initial Commit

---
+++ swiftbook_3_2_urlSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook_3_2_urlSession
+//
+//  Created by dobrets on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
